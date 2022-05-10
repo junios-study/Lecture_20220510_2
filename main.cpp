484f4726931789a345617644949f635a28f6d551
junios <junios@naver.com>
2022-05-10 17:17:23 +0900

    맵 그리기

--- main.cpp
+++ main.cpp
@@ -6,58 +6,96 @@ using namespace std;
 
 struct FVector2D
 {
-    int X;
-    int Y;
+	int X;
+	int Y;
 };
+
+char Map[10][10] = {
+	{'#','#','#','#','#','#','#','#','#','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ','#',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ','#',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#','#','#','#','#','#','#','#','#','#'}
+};
+
 
 void SetLocation(FVector2D NewLocation);
 
+void DrawBG(FVector2D StartPosition)
+{
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			FVector2D Temp;
+			Temp.X = X + StartPosition.X;
+			Temp.Y = Y + StartPosition.Y;
+			SetLocation(Temp);
+			cout << Map[Y][X];
+		}
+	}
+
+}
 
 int main()
 {
-    bool bRunning = true;
-    FVector2D PlayerPosition;
-    PlayerPosition.X = 0;
-    PlayerPosition.Y = 0;
+	bool bRunning = true;
+	FVector2D PlayerPosition;
+	PlayerPosition.X = 0;
+	PlayerPosition.Y = 0;
 
-    while (bRunning)
-    {
-        int KeyCode = _getch();
+	srand(static_cast<unsigned int>(time(nullptr)));
+	FVector2D StartPosition;
+	StartPosition.X = rand() % 10 + 1;
+	StartPosition.Y = rand() % 10 + 1;
 
-        switch (KeyCode)
-        {
-        case 'w':
-        case 'W':
-            PlayerPosition.Y--;
-            break;
-        case 's':
-        case 'S':
-            PlayerPosition.Y++;
-            break;
-        case 'a':
-        case 'A':
-            PlayerPosition.X--;
-            break;
-        case 'd':
-        case 'D':
-            PlayerPosition.X++;
-            break;
-        case 27:
-            bRunning = false;
-            break;
+	while (bRunning)
+	{
+		int KeyCode = _getch();
 
-        }
+		switch (KeyCode)
+		{
+		case 'w':
+		case 'W':
+			PlayerPosition.Y--;
+			break;
+		case 's':
+		case 'S':
+			PlayerPosition.Y++;
+			break;
+		case 'a':
+		case 'A':
+			PlayerPosition.X--;
+			break;
+		case 'd':
+		case 'D':
+			PlayerPosition.X++;
+			break;
+		case 27:
+			bRunning = false;
+			break;
 
-        PlayerPosition.X = PlayerPosition.X < 0 ? 0 : PlayerPosition.X;
-        PlayerPosition.Y = PlayerPosition.Y < 0 ? 0 : PlayerPosition.Y;
-        PlayerPosition.X = PlayerPosition.X > 51 ? 51 : PlayerPosition.X;
-        PlayerPosition.Y = PlayerPosition.Y > 14 ? 14 : PlayerPosition.Y;
+		}
 
-        system("cls");
+		PlayerPosition.X = PlayerPosition.X < 1 ? 1 : PlayerPosition.X;
+		PlayerPosition.Y = PlayerPosition.Y < 1 ? 1 : PlayerPosition.Y;
+		PlayerPosition.X = PlayerPosition.X >= 9 ? 8 : PlayerPosition.X;
+		PlayerPosition.Y = PlayerPosition.Y >= 9 ? 8 : PlayerPosition.Y;
 
-        SetLocation(PlayerPosition);
-        cout << "P";
-    }
+		system("cls");
+
+		DrawBG(StartPosition);
+		FVector2D Temp;
+		Temp.X = PlayerPosition.X + StartPosition.X;
+		Temp.Y = PlayerPosition.Y + StartPosition.Y;
+		SetLocation(Temp);
+		cout << "P";
+	}
 
 
 	return 0;
@@ -66,8 +104,8 @@ int main()
 
 void SetLocation(FVector2D NewLocation)
 {
-    COORD Cur;
-    Cur.X = NewLocation.X;
-    Cur.Y = NewLocation.Y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+	COORD Cur;
+	Cur.X = NewLocation.X;
+	Cur.Y = NewLocation.Y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
 }
